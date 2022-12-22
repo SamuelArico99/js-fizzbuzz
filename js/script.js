3874f6034d075b43a059533baae462ad3bf8c27b
SamuelArico99 <samuel16699.sa@gmail.com>
2022-12-22 09:55:50 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//  Sfruttare ciclo for per conteggio da 1 a 100
+// Per ogni numero:
+// -Controllo se è multiplo di 3 OPPURE
+// --Se è multiplo di 3 stampo Fizz
+
+// -Controllo se è multiplo di 5 OPPURE
+// --Se è multiplo di 5 stampo Buzz
+
+// --Controllo se è multiplo di 3 e di 5 OPPURE
+// --se è multiplo sia di 3 che di 5 stampo fizzbuzz
+
+// --Se non è multiplo di 3 né di 5 
+// ---stampo il numero che sto analizzando
+
+
+// FOR
+
+for (let i = 1; i <= 100; i++) {
+    console.log('i', i,typeof i)
+    
+}
